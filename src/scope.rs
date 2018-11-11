6b0d9b3318fc943c8ef5685034d10afae41ecac9
Niko Lepka <slench102+git@gmail.com>
2018-11-11 17:03:56 +0100

    add SymbolTable and ScopeTable types

--- src/scope.rs
+++ src/scope.rs
@@ -1,5 +1,12 @@
+use std::collections::HashMap;
 use uuid::Uuid;
-use token::Stack;
+use token::{Token, Stack};
+
+/// Stores the name of a symbol and the associated data stored within
+pub type SymbolTable = HashMap<String, Token>;
+
+/// Stores the symbol table of each scope
+pub type ScopeTable = HashMap<Uuid, SymbolTable>;
 
 pub struct Scope {
     parent: Option<Uuid>,
