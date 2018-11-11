85e49eebc680a35d7c85e1d2e848c0946c3dab7a
Niko Lepka <slench102+git@gmail.com>
2018-11-11 17:04:16 +0100

    make scope add itself to the scope table

--- src/scope.rs
+++ src/scope.rs
@@ -15,15 +15,28 @@ pub struct Scope {
 }
 
 impl Scope {
-    fn new(parent: Option<Uuid>) -> Scope {
+    fn new(scopes: &mut ScopeTable, parent: Option<Uuid>) -> Scope {
+        let mut id = Uuid::new_v4();
+
+        // if the newly created scope's ID is a name clash, try again
+        if scopes.get(&id).is_some() {
+            id = Uuid::new_v4();
+        }
+
+        scopes.insert(id, SymbolTable::new());
+
         Scope {
             parent,
-            id: Uuid::new_v4(),
+            id: id,
             stack: Stack::new(),
         }
     }
 
     fn has_parent(&self) -> bool {
         self.parent.is_some()
+    }
+
+    fn id(&self) -> Uuid {
+        self.id
     }
 }
