ae31b6e5ad80d08190f4c56601a144283d4156a6
Niko Lepka <slench102+git@gmail.com>
2018-11-11 18:41:50 +0100

    move everything from lexer to parser
    what lexer did had nothing to do with lexing

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,36 @@
+use std::collections::HashMap;
+use std::ops::*; // adds math operations to f64
 use token::{Stack, Token};
-use lexer;
-use error::{self, Error};
+use error::{Error, Result};
+
+type Bif = fn(&[Token]) -> Result<Token>; // short for Built-In Function
+
+pub struct Callback {
+    pub arity: usize,
+    pub func: Bif,
+}
+
+lazy_static! {
+    pub static ref BIFS: HashMap<&'static str, Callback> = {
+        let mut map: HashMap<&'static str, Callback> = HashMap::new();
+        map.insert("+",   Callback { arity: 2, func: |args| apply_num_binop(args, f64::add)});
+        map.insert("-",   Callback { arity: 2, func: |args| apply_num_binop(args, f64::sub)});
+        map.insert("*",   Callback { arity: 2, func: |args| apply_num_binop(args, f64::mul)});
+        map.insert("/",   Callback { arity: 2, func: |args| apply_num_binop(args, f64::div)});
+        map.insert("neg", Callback { arity: 1, func: |args| apply_num_unop(args, f64::neg)});
+        map.insert("sin", Callback { arity: 1, func: |args| apply_num_unop(args, f64::sin)});
+        map.insert("cos", Callback { arity: 1, func: |args| apply_num_unop(args, f64::cos)});
+        map.insert("tan", Callback { arity: 1, func: |args| apply_num_unop(args, f64::tan)});
+        //map.insert("copy", |args| )
+        map.insert("if", Callback { arity: 3, func: handle_if});
+        map
+    };
+}
 
 /// Parses an n-ary operator
-pub fn parse_nary(stack: &mut Stack, symbol: &str) -> error::Result<()> {
+pub fn parse_nary(stack: &mut Stack, symbol: &str) -> Result<()> {
     // get the callback stored in BIFS, if available
-    let callback = lexer::BIFS.get(symbol).ok_or(Error::UnknownIdentifier)?;
+    let callback = BIFS.get(symbol).ok_or(Error::UnknownIdentifier)?;
 
     let mut args = Vec::new();
 
@@ -32,4 +57,52 @@ pub fn parse_nary(stack: &mut Stack, symbol: &str) -> error::Result<()> {
     }
 
     Ok(())
+}
+
+/// Checks if the expected arity matches the actual one.
+/// Returns Error::ArityMismatch if it doesn't, and nothing if all is fine.
+fn check_arity(expected: usize, actual: usize) -> Result<()> {
+    if actual == expected {
+        Ok(())
+    } else {
+        Err(Error::ArityMismatch)
+    }
+}
+
+/// Applies a binary numeric operation and returns the resulting token.
+fn apply_num_binop(args: &[Token], func: fn(f64, f64) -> f64) -> Result<Token> {
+    check_arity(2, args.len())?;
+
+    let a = args[0].get_number()?;
+    let b = args[1].get_number()?;
+
+    let result = func(a, b);
+
+    Ok(Token::Number(result))
+}
+
+/// Applies a unary numeric operation and returns the resulting token.
+fn apply_num_unop(args: &[Token], func: fn(f64) -> f64) -> Result<Token> {
+    check_arity(1, args.len())?;
+
+    let a = args[0].get_number()?;
+    let result = func(a);
+
+    Ok(Token::Number(result))
+}
+
+/// Applies an if-statement operation and returns the corresponding token
+fn handle_if(args: &[Token]) -> Result<Token> {
+    check_arity(3, args.len())?;
+
+    // if-statements are written [then] [else] cond if
+    let then_case = args[0].clone();
+    let else_case = args[1].clone();
+    let condition = args[2].get_boolean()?;
+
+    if condition {
+        Ok(then_case)
+    } else {
+        Ok(else_case)
+    }
 }
