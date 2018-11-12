663edb8992f12935fff18fa4046ba83915c3967b
Niko Lepka <slench102+git@gmail.com>
2018-11-12 10:43:46 +0100

    add clap

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@ use std::io;
 #[macro_use]
 extern crate lazy_static;
 extern crate uuid;
+#[macro_use]
+extern crate clap;
 
 #[cfg(test)] mod test;
 mod lexer;
@@ -16,6 +18,9 @@ fn main() -> io::Result<()> {
     let mut scopes = tables::ScopeTable::new();
     let mut main_scope = scope::Scope::new(&mut scopes, None);
     
+    let yaml = load_yaml!("cli.yaml");
+    let matches = clap::App::from_yaml(yaml).get_matches();
+
     println!("Hello, world!");
 
     Ok(())
