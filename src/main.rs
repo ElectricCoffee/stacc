46fea14927624d9a4dd8748dc2bb81b9e4278e51
Niko Lepka <slench102+git@gmail.com>
2018-11-12 11:01:46 +0100

    read file in main by use of CLAP

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io;
+use std::fs;
 
 #[macro_use]
 extern crate lazy_static;
@@ -14,14 +15,27 @@ mod error;
 mod scope;
 mod tables;
 
+fn read_file(matches: clap::ArgMatches) -> io::Result<String> {
+    use io::{self, ErrorKind, Read};
+    let filename = matches
+        .value_of("INPUT")
+        .ok_or(io::Error::new(ErrorKind::NotFound, "Could not find the file"))?;
+    let mut file = fs::File::open(filename)?;
+    let mut contents = String::new();
+    
+    file.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
 fn main() -> io::Result<()> {
     let mut scopes = tables::ScopeTable::new();
     let mut main_scope = scope::Scope::new(&mut scopes, None);
     
     let yaml = load_yaml!("cli.yaml");
     let matches = clap::App::from_yaml(yaml).get_matches();
+    let file = read_file(matches)?;
 
-    println!("Hello, world!");
+    println!("Contents of the file: {}", file);
 
     Ok(())
 }
