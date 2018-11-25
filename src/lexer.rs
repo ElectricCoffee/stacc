fd11097037e10077220e423f05c0eccc9a4e9b78
Niko Lepka <slench102+git@gmail.com>
2018-11-25 11:50:21 +0100

    add basic lex function to lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,4 +1,5 @@
 use regex::Regex;
+use token::Token;
 
 lazy_static!{
     pub static ref NUMBER: Regex      = Regex::new(r"(\d*\.\d+|\d+)").unwrap();
@@ -8,4 +9,9 @@ lazy_static!{
     pub static ref END_SCOPE: Regex   = Regex::new(r"\)").unwrap();
     pub static ref BEGIN_LIST: Regex  = Regex::new(r"\[").unwrap();
     pub static ref END_LIST: Regex    = Regex::new(r"\]").unwrap();
+}
+
+/// Takes the input string and returns a vec of tokens.
+pub fn lex(input: &str) -> Vec<Token> {
+    unimplemented!();
 }
